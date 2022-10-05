4dbec99f2d588389d0010dca2655fc7129b95527
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-10-04 19:16:09 -0700

    Set Interval

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction=null;
+let direction = null;
 let x = 100;
 let y = 250;
-if (direction ==='west'){
+
+setInterval(function()){
+if (direction === 'west'){
     x = x - 1
 }
+if (direction === 'north'){
+    y = y + 1
+}
+if(direction === 'east'){
+    x = x + 1
+}
+if (direction === 'south'){
+    y = y - 1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+}, 1
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
