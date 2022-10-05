3ceae0823044c7ad83b675aabec17aa1b854246b
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-10-04 19:21:27 -0700

    added Eventlistener

--- index.js
+++ index.js
@@ -6,22 +6,20 @@ let direction = null;
 let x = 100;
 let y = 250;
 
-setInterval(function()){
-if (direction === 'west'){
-    x = x - 1
-}
-if (direction === 'north'){
-    y = y + 1
-}
-if(direction === 'east'){
-    x = x + 1
-}
-if (direction === 'south'){
-    y = y - 1
-}
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
-}, 1
+document.addEventListene('keydown', function(e){
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
 
 
 
